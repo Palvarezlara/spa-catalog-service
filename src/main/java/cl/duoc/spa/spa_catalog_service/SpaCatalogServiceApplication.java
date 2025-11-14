d391412b0fee94d2fddcfe5a7d50bc76e7e27f92
PalvarezLara <pame.alvarez@duocuc.cl>
2025-11-13 22:34:59 -0300

    Initial Spring Boot project commit

---
+++ src/main/java/cl/duoc/spa/spa_catalog_service/SpaCatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.spa.spa_catalog_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaCatalogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaCatalogServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/duoc/spa/spa_catalog_service/SpaCatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.spa.spa_catalog_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaCatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
